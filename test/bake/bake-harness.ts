f73d45e7943f6ea3fb3d1b39ea1e39b823b688a9
Alistair Smith <hi@alistair.sh>
2025-10-02 19:06:40 -0700

    changes

--- test/bake/bake-harness.ts
+++ test/bake/bake-harness.ts
@@ -2044,7 +2044,7 @@ devTest.only = function (description: string, options: DevServerTest) {
   return testImpl(description, { ...options, only: true }, "development", caller);
 };
 
-export function prodTest<T extends DevServerTest>(description: string, options: T): T {
+export function prodTest(description: string, options: DevServerTest) {
   const callerLocation = snapshotCallerLocation();
   const caller = stackTraceFileName(callerLocation);
   assert(
